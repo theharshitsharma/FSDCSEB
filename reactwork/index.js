9e40430828f3023dd5a0285882bd96d779614c88
theharshitsharma <harshit2004ji@gmail.com>
2024-11-07 11:49:20 +0530

    react

---
+++ reactwork/index.js
@@ -0,0 +1,23 @@
+// console.log("hello world");
+// const div=document.getElementById("parent")
+// console.dir(div)
+// const h2=document.createElement("h2")
+// h2.innerText="welcome to react js"
+// h2.style.color='red'
+// console.log(h2)
+// div.appendChild(h2)
+
+const parent=document.getElementById("root")
+const root=ReactDOM.createRoot(parent)
+// const h2=React.createElement("h2",{style:{color:'cyan'}},"Welcome to react js")
+// const li1=React.createElement("li",{style:{backgroundColor:"red"}},"Orange")
+// const li2=React.createElement("li",{style:{color:"blue"}},"Apple")
+// const li3=React.createElement("li",{style:{color:"orange"}},"banana")
+// const li4=React.createElement("li",{style:{color:"blue"}},"Mango")
+// const li5=React.createElement("li",{style:{color:"purple"}},"Grapes")
+
+// const ul=React.createElement("ul",{style:{backgroundColor:"cyan"}},li1,li2,li3,li4,li5)
+
+const h1=React.createElement("h1",{},"by react create element")
+root.render(ul);
+
